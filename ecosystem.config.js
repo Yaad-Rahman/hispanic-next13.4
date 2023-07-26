f962b47d334c4f7690964a980e3ac710c2f7f6f0
yaad-rahman <yaadrahman95@gmail.com>
2023-07-26 11:08:08 +0600

    fix: ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   apps: [
     {
-      name: "hispanic-web",
-      exec_mode: "cluster",
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
+      name: 'hispanic-web',
+      exec_mode: 'cluster',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
       watch: true,
       env_development: {
         PORT: 6969,
-        NODE_ENV: "development",
+        NODE_ENV: 'development',
       },
       env_production: {
         PORT: 6969,
-        NODE_ENV: "production",
+        NODE_ENV: 'production',
       },
     },
   ],
