34d68c14f7d181e6e5013a91203c429c74e2885e
yaad rahman <yaadrahman95@gmail.com>
2023-09-03 16:26:43 +0600

    fix: env git ignore

--- app/(normal-layout)/board/page.tsx
+++ app/(normal-layout)/board/page.tsx
@@ -8,31 +8,35 @@ import type { BoardPageProps } from '@/types/boardType';
 export default async function Board() {
   const memberCategories = await getAllBoardMemberCategories();
 
-  const categories = memberCategories.payload.content.map((category) => ({
-    id: category.id,
-    name: category.name,
-  }));
+  const categories =
+    memberCategories.payload?.content &&
+    memberCategories.payload.content.length > 0 &&
+    memberCategories.payload.content.map((category) => ({
+      id: category.id,
+      name: category.name,
+    }));
 
-  const dataPromises = categories.map(async (category) => {
-    const membersResponse = await getAllBoardMembers(category.id);
-    if (
-      membersResponse.payload?.content &&
-      membersResponse.payload?.content.length > 0
-    ) {
-      return {
-        category: category.name,
-        members: membersResponse.payload.content,
-      };
-    }
+  const dataPromises =
+    categories &&
+    categories.map(async (category) => {
+      const membersResponse = await getAllBoardMembers(category.id);
+      if (
+        membersResponse.payload?.content &&
+        membersResponse.payload?.content.length > 0
+      ) {
+        return {
+          category: category.name,
+          members: membersResponse.payload?.content,
+        };
+      }
 
-    return null;
-  });
+      return null;
+    });
 
-  const data = await Promise.all(dataPromises);
+  const data = dataPromises && (await Promise.all(dataPromises));
 
-  const filteredData = data.filter(
-    (item) => item !== null
-  ) as BoardPageProps['data'];
+  const filteredData =
+    data && (data.filter((item) => item !== null) as BoardPageProps['data']);
 
-  return <BoardPage data={filteredData} />;
+  return <BoardPage data={filteredData || []} />;
 }
