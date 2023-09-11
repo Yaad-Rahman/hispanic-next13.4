bc5406b31f79664b82f40460ecdceb994c679198
yaad-rahman <yaadrahman95@gmail.com>
2023-09-11 18:27:54 +0600

    feat: user profile added

--- components/layout/Navbar/LoggedInSection.tsx
+++ components/layout/Navbar/LoggedInSection.tsx
@@ -1,4 +1,5 @@
-import { Button, Typography } from '@hispanic-ui';
+import { Button } from '@hispanic-ui';
+import { useRouter } from 'next/navigation';
 import { useDispatch } from 'react-redux';
 
 import { logoutUser } from '@/redux/slices/authSlice';
@@ -6,6 +7,7 @@ import type { UserType } from '@/types/authType';
 
 export const LoggedInSection = ({ user }: { user: UserType }) => {
   const dispatch = useDispatch();
+  const router = useRouter();
   return (
     <div className="flex items-center gap-5">
       <img
@@ -13,9 +15,15 @@ export const LoggedInSection = ({ user }: { user: UserType }) => {
         alt="profile"
         className="h-10 w-10 rounded-full object-cover"
       />
-      <Typography variant="white" weight="semibold">
-        {user.firstName}
-      </Typography>
+      <Button
+        onClick={() => {
+          router.push('/profile');
+        }}
+        variant="text"
+        label={user.firstName}
+        className="!font-bold text-white"
+      />
+
       <Button
         onClick={() => {
           dispatch(logoutUser());
